148c88b19b8b47365ec9e67d3b06a4a9636b5770
gadyez <gady@payboxapp.com>
2017-06-03 12:54:01 +0300

    header fix

--- app.js
+++ app.js
@@ -13,6 +13,14 @@ app.use(express.static(__dirname + '/'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
+app.use(
+    function (req,res,next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.set("Content-Type", "application/json");
+    next();
+});
+
 app.listen(port);
 
 app.get('/', function(req,res) {
